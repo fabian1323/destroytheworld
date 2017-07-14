bc2e3818222dbdf1a6eb7c02596eac790e67e8da
Fabian <fabianbleeksma.apps@gmail.com>
2017-07-14 22:02:40 +0200

    Test2

--- DestroyTheWorld/ViewController.swift
+++ DestroyTheWorld/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     
-    print("test")
+    print("test2")
   }
 
   override func didReceiveMemoryWarning() {
